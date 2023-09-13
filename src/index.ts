cd578d46621febb6afcc55584d1131fd2ab6e662
DEE <donzydonathan@gmail.com>
2023-09-13 23:14:43 +0100

    feat: complete basic next-prime func

--- src/index.ts
+++ src/index.ts
@@ -12,12 +12,28 @@ program
     "Input a number and I will continue to prompt you with the next prime number"
   );
 
-program
-  .argument("<number>", "the first number")
-  .action((number: string, options) => {
-    console.log(number, options);
-  });
+program.argument("<number>", "the first number").action((number: string) => {
+  console.log(nextPrime(parseInt(number)));
+});
 
 program.parse();
 
 // next prime function
+function nextPrime(n: number) {
+  n % 2 !== 0 ? (n += 2) : n++;
+  while (!isPrime(n)) {
+    if (n % 2 !== 0) {
+      n += 2;
+    } else {
+      n++;
+    }
+  }
+  return n;
+}
+
+function isPrime(n: number) {
+  for (let i = 2; i * i <= n; i++) {
+    if (n % i === 0) return false;
+  }
+  return true;
+}
